d88960540d2d1b8be986e60dbdf329109aa418dd
Ghaem Elahi <ghaemelahi.7913@gmail.com>
2024-11-29 12:57:35 +0330

    chor: add check login user with access token to controllers

--- backend/app/Http/Controllers/v1/api/TagController.php
+++ backend/app/Http/Controllers/v1/api/TagController.php
@@ -7,48 +7,75 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class TagController extends Controller
 {
     public function index(Request $request)
     {
-        $validate = Validator::make($request->all(), [
-            "search" => "nullable",
-        ]);
+        $accessToken = request()->cookie('access_token');
 
-        if ($validate->fails()) {
+        if ($accessToken) {
+            $accessTokenModel = PersonalAccessToken::findToken($accessToken);
+            if ($accessTokenModel) {
+                $userId = $accessTokenModel->tokenable_id;
+            }
+            $validate = Validator::make($request->all(), [
+                "search" => "nullable",
+            ]);
+
+            if ($validate->fails()) {
+                return response()->json([
+                    'status' => 401,
+                    "messages" => $validate->errors(),
+                ], 401);
+            }
+            $data["list_tags"] = DB::table('tags')->where("tag", "LIKE", "%$request->search%")->get();
+
             return response()->json([
-                'status' => 401,
-                "messages" => $validate->errors(),
-            ],401);
+                'status' => 200,
+                "data" => $data["list_tags"],
+            ], 200);
+        } else {
+            return response()->json([
+                "status" => 401,
+                "message" => "please login to your account",
+            ], 401);
         }
-
-        $data["list_tags"] = DB::table('tags')->where("tag", "LIKE", "%$request->search%")->get();
-
-        return response()->json([
-            'status' => 200,
-            "data" => $data["list_tags"],
-        ],200);
     }
     public function store(Request $request)
     {
-        $validate = Validator::make($request->all(), [
-            'tag' => "required|string|unique:tags",
-        ]);
-        if ($validate->fails()) {
+        $accessToken = request()->cookie('access_token');
+
+        if ($accessToken) {
+            $accessTokenModel = PersonalAccessToken::findToken($accessToken);
+            if ($accessTokenModel) {
+                $userId = $accessTokenModel->tokenable_id;
+            }
+            $validate = Validator::make($request->all(), [
+                'tag' => "required|string|unique:tags",
+            ]);
+            if ($validate->fails()) {
+                return response()->json([
+                    'status' => 401,
+                    "error_message" => $validate->errors(),
+                ], 401);
+            }
+
+            DB::table('tags')->insert([
+                'id' => str::uuid(),
+                'tag' => $request->tag
+            ]);
+
             return response()->json([
-                'status' => 401,
-                "error_message" => $validate->errors(),
-            ],401);
+                'status' => 201,
+                'messages' => 'tag created successfully.'
+            ], 201);
+        } else {
+            return response()->json([
+                "status" => 401,
+                "message" => "please login to your account",
+            ], 401);
         }
-
-        DB::table('tags')->insert([
-            'id' => str::uuid(),
-            'tag' => $request->tag
-        ]);
-
-        return response()->json([
-            'status' => 201,
-        ],201);
     }
 }

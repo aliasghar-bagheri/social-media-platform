8f5dd82ebe1de20267a26703537575f233f1b8f4
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2024-12-02 15:50:10 +0330

    feat(spinner): add animation class to spinner

--- frontend/src/components/ui/Spinner.tsx
+++ frontend/src/components/ui/Spinner.tsx
@@ -3,8 +3,14 @@ import { forwardRef, HTMLAttributes } from "react";
 const Spinner = forwardRef<HTMLSpanElement, HTMLAttributes<HTMLSpanElement>>(
   ({ className, ...props }, ref) => {
     return (
-      <span className={` ${className}`} ref={ref} {...props}>
-        <img src="/assets/icons/loader.svg" width={23} height={23} alt="spinner" />
+      <span className={`${className}`} ref={ref} {...props}>
+        <img
+          src="/assets/icons/loader.svg"
+          className="animate-spin"
+          width={23}
+          height={23}
+          alt="spinner"
+        />
       </span>
     );
   },

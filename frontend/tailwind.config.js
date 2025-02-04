ec63233825c77c09c9268f96cdd2fea02d027488
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-04 17:32:03 +0330

    fix: add the tailwindcss animate package

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -65,5 +66,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
